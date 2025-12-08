030cc5877673ec1f47d642bff62c78e3e9490c6d
ReoAndBoof <77257116+ReoAndBoof@users.noreply.github.com>
2025-12-08 09:26:49 +0900

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 # app/routes/__init__.py
 
 from .booking import booking_bp
+from .test00 import test00_bp
 
 def register_routes(app):
     app.register_blueprint(booking_bp)
+    app.register_blueprint(test00_bp)
